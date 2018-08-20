f2e24ecb94b76f0e49ac3fb650ab249b132f61d0
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-08-20 18:40:39 +0100

    chore: move from TS to Flow (#7)
    Moved from SCSS to Styled components

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// @flow
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.render(<App />, rootElement);
+}
+registerServiceWorker();
