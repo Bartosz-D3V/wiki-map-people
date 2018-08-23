d4f7bc568b7e307a38d390c1239305b6daeeb2b5
bartosz.wyporkiewicz <bartosz.wyporkiewicz@servelec-corelogic.co.uk>
2018-08-23 13:21:26 +0100

    feat: implement first draft of autocomplete

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -2,10 +2,20 @@
 import React from 'react';
 import Select from 'react-select';
 import NoSsr from '@material-ui/core/NoSsr';
+import styled from 'styled-components';
+import Countries from './Countries';
+import { withStyles } from '@material-ui/core/styles';
+import SubComponents from './SubComponents';
+import SubComponentsClasses from './SubComponentsClasses';
 
 type T = {};
-export default class extends React.Component<T> {
-  static getCountries(input: string, countries: Array<Object>): Array<Object> {
+const SelectWrapper = styled.div`
+  display: flex;
+  flex-flow: row;
+  justify-content: flex-start;
+`;
+class CountryList extends React.Component<T> {
+  static getCountries(input?: string, countries: Array<Object>): Array<Object> {
     return countries.filter(
       (val: Object): boolean =>
         !!input && val.label.toLowerCase().indexOf(input.toLowerCase()) !== -1
@@ -13,10 +23,29 @@ export default class extends React.Component<T> {
   }
 
   render() {
+    const { classes, theme } = this.props;
+
+    const selectStyles = {
+      input: base => ({
+        ...base,
+        color: theme.palette.text.primary,
+      }),
+    };
+
     return (
-      <NoSsr>
-        <Select placeholder="Nationality" />
-      </NoSsr>
+      <SelectWrapper>
+        <NoSsr>
+          <Select
+            classes={classes}
+            styles={selectStyles}
+            options={Countries}
+            components={SubComponents}
+            onChange={() => CountryList.getCountries(Countries)}
+            placeholder="Search a country"
+          />
+        </NoSsr>
+      </SelectWrapper>
     );
   }
 }
+export default withStyles(SubComponentsClasses, { withTheme: true })(CountryList);
---
+++ src/components/CountryList/SubComponentsClasses.js
@@ -0,0 +1,31 @@
+import { emphasize } from '@material-ui/core/styles/colorManipulator';
+
+export default theme => ({
+  input: {
+    display: 'flex',
+    padding: 0,
+  },
+  valueContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flex: 1,
+    alignItems: 'center',
+  },
+  noOptionsMessage: {
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+  },
+  singleValue: {
+    fontSize: 16,
+  },
+  placeholder: {
+    position: 'absolute',
+    left: 2,
+    fontSize: 16,
+  },
+  paper: {
+    marginTop: theme.spacing.unit,
+  },
+  divider: {
+    height: theme.spacing.unit * 2,
+  },
+});
