7c5caa066a802ad6bb4eecc2357f96e9e9fadb86
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-08-27 20:24:08 +0100

    optimize query

--- packages/server/query/getPeople.js
+++ packages/server/query/getPeople.js
@@ -2,17 +2,17 @@ export default country => `
   PREFIX wd: <http://www.wikidata.org/entity/>
   PREFIX wdt: <http://www.wikidata.org/prop/direct/>
   PREFIX schema: <http://schema.org/>
-
-  SELECT  DISTINCT ?id ?idLabel ?pobLabel ?citizenshipLabel ?article  WHERE {
+   SELECT  DISTINCT ?fullName ?placeOfBirth ?article ?coords  WHERE {
   ?id wdt:P31 wd:Q5.
   ?id wdt:P19 ?pob.
   ?id wdt:P27 ?citizenship.
+  ?pob wdt:P625 ?coords.
   ?article schema:about ?id.
   ?article schema:isPartOf <https://en.wikipedia.org/>.
   SERVICE wikibase:label {
     bd:serviceParam wikibase:language "en".
-    ?id rdfs:label ?idLabel .
-    ?pob rdfs:label ?pobLabel.
+    ?id rdfs:label ?fullName .
+    ?pob rdfs:label ?placeOfBirth.
     ?citizenship rdfs:label ?citizenshipLabel.
   } ?citizenship ?label "${country}".
-  }`;
+}`;
