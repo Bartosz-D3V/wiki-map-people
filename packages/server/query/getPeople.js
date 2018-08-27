2231578dfc818726a873952690c6ec24067081d2
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-08-27 18:40:03 +0100

    feat: get list of people - SPARQL

---
+++ packages/server/query/getPeople.js
@@ -0,0 +1,20 @@
+export default country => {
+  return `
+  PREFIX wd: <http://www.wikidata.org/entity/>
+  PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+  PREFIX schema: <http://schema.org/>
+
+  SELECT  DISTINCT ?id ?idLabel ?pobLabel ?citizenshipLabel ?article  WHERE {
+  ?id wdt:P31 wd:Q5.
+  ?id wdt:P19 ?pob.
+  ?id wdt:P27 ?citizenship.
+  ?article schema:about ?id.
+  ?article schema:isPartOf <https://en.wikipedia.org/>.
+  SERVICE wikibase:label {
+    bd:serviceParam wikibase:language "en".
+    ?id rdfs:label ?idLabel .
+    ?pob rdfs:label ?pobLabel.
+    ?citizenship rdfs:label ?citizenshipLabel.
+  } ?citizenship ?label "${country}".
+  }`;
+};
