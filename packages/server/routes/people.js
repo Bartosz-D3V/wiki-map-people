6ea23030500a0d27aee96402b50882497968a61e
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-08-30 14:54:19 +0100

    feat: finish controller method (#22)

--- packages/server/routes/people.js
+++ packages/server/routes/people.js
@@ -1,11 +1,31 @@
 import express from 'express';
-import http from 'http';
+import https from 'https';
+
+import getPeople from '../query/getPeople';
+import getFetchURL from '../query/getFetchURL';
 
 const router = express.Router();
 
 /* GET people listing by nationality. */
 router.get('/:country', (req, res) => {
   const country = req.param('country');
+  const query = getPeople(country);
+  const fetchURL = getFetchURL(query);
+
+  let data;
+  https.get(fetchURL, response => {
+    response.on('data', chunk => {
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      res.json(data).status(200);
+    });
+
+    response.on('error', err => {
+      res.send(err).status(500);
+    });
+  });
 });
 
 export default router;
