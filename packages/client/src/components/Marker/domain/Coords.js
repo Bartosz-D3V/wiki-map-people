6442c5b18393ce0e54905ddba553942f98562d8d
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-09-04 10:45:45 +0100

    feat(client): function to format coordinates from wikidata

---
+++ packages/client/src/components/Marker/domain/Coords.js
@@ -0,0 +1,12 @@
+// @flow
+
+export default class {
+  latitude: number;
+
+  longtitude: number;
+
+  constructor(latitude: number, longtitude: number) {
+    this.latitude = latitude;
+    this.longtitude = longtitude;
+  }
+}
