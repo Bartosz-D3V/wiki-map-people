694840bfd71170e16d98fa1b5521d362db8fe311
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-09-10 20:06:19 +0100

    render markers

---
+++ packages/client/src/components/MapWrapper/MapWrapper.jsx
@@ -0,0 +1,43 @@
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import PersonalInfo from '../../domain/PersonalInfo';
+
+const LeafletWrapper = styled(Map)`
+  height: 100vh;
+  width: 100vw;
+  z-index: 1;
+`;
+const mapCenter = [39.9528, -75.1638];
+const zoomLevel = 12;
+const stamenTonerTiles = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+
+declare type T = {
+  associatedPeople: Array<PersonalInfo>,
+};
+
+export class MapWrapper extends React.Component<T> {
+  static getMarkers(associatedPeople: Array<PersonalInfo>): any {
+    if (!associatedPeople) return null;
+    return associatedPeople.map((personalInfo: PersonalInfo) => {
+      const { latitude, longtitude } = personalInfo.coords;
+      return (
+        <Marker key={personalInfo.article} position={[latitude, longtitude]}>
+          <Popup>Test</Popup>
+        </Marker>
+      );
+    });
+  }
+
+  render() {
+    const { associatedPeople } = this.props;
+
+    return (
+      <LeafletWrapper center={mapCenter} zoom={zoomLevel}>
+        <TileLayer url={stamenTonerTiles} />
+        {MapWrapper.getMarkers(associatedPeople)};
+      </LeafletWrapper>
+    );
+  }
+}
