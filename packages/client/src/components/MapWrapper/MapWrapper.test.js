9df3eb772cef9d1fbb8371c39a7d2b6c8c4973f7
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-09-17 09:44:00 +0100

    fix issues

--- packages/client/src/components/MapWrapper/MapWrapper.test.js
+++ packages/client/src/components/MapWrapper/MapWrapper.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MapWrapper from './MapWrapper';
 import PersonalInfo from 'shared/domain/PersonalInfo';
 import Coords from 'shared/domain/Coords';
+import MapWrapper from './MapWrapper';
 
 describe('MapWrapper', () => {
   const person1 = new PersonalInfo('Donnie Darko', 'USA', new Coords(1, 1), 'localhost');
