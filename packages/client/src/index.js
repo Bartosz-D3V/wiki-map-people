1b85fceddea179e3fa022d5cb3b131849ca51d5c
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2019-03-31 11:05:40 +0100

    fix: PROD env param (#51)

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -9,7 +9,9 @@ import registerServiceWorker from './registerServiceWorker';
 const rootElement = document.getElementById('root');
 
 const getBaseUrl = () =>
-  process.env.mode === 'prod' ? process.env.REACT_APP_PUBLIC_URL : 'http://localhost:8080';
+  process.env.REACT_APP_MODE === 'prod'
+    ? process.env.REACT_APP_PUBLIC_URL
+    : 'http://localhost:8080';
 
 if (rootElement) {
   ReactDOM.render(<App baseUrl={getBaseUrl()} />, rootElement);
